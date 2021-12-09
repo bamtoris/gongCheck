45a64cae5d375eb7b06c15f4f0b59be26d9c4ad1
LeeJongMoon <woobi4966@naver.com>
2021-12-09 21:53:41 +0900

    add reset.css on App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import "./components/reset.css"; // reset for css default settings
+import "./reset.css"; // reset for css default settings
 
 import Header from "./components/Header";
 import {Routes, Route} from 'react-router-dom';
 
-
-
-
 import MyPage from "./pages/myPage";
 import Community from "./pages/Community";
+
 
 
 
